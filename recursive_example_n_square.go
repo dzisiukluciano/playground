738b54a026d2c7fa76e11b95b60dc7bae12ab647
Luciano Dzisiuk <dzisiukluciano@gmail.com>
2021-11-14 00:25:37 -0300

    Update recursive_example_n_square.go

--- recursive_example_n_square.go
+++ recursive_example_n_square.go
@@ -18,7 +18,7 @@ func fn(n int, it *int) {
 
 // example of worst case scenario of quick sort, where we always choose the greatest element,
 // therefore dividing in sub-arrays with n-1 elements
-// technically, time complexity is O(n^2) because is n+(n-1)+(n-2)+...+1 = Sum(1 to n) of n = n*(n-1)/2 =~ (1/2)(n*n) =~ n^2
+// technically, time complexity is O(n^2) because is n+(n-1)+(n-2)+...+1 = Sum(1 to n) of n = n*(n+1)/2 =~ (1/2)(n*n) =~ n^2
 func main() {
 	it := 0
 	n := 10
